0e2816a669740e90774699bdfb0759e7754d0171
JohnEsleyer <ralphjohnpolicarpio@outlook.com>
2024-03-16 09:46:33 +0800

    update main.go

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
-}
-
 func main() {
-	http.HandleFunc("/hello", helloHandler)
-	fmt.Println("Starting server on port 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Printf("Error starting server: %s\n", err)
-	}
+	// Serve files from the "static" directory
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	// Start the server
+	http.ListenAndServe(":8080", nil)
 }
